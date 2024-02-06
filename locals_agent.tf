41344c8f4a8e084a84658785efffea1ab0de1594
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-06 11:37:30 -0500

    v0.0.22

--- locals_agent.tf
+++ locals_agent.tf
@@ -3,9 +3,10 @@ locals {
   # The following locals use logic to determine the required agent pools.
   agent_pools = flatten([for project_key, project in local.projects :
     flatten([for workspace_key, workspace in project.workspaces :
-      workspace.tfc_workspace.agent_pool if workspace.tfc_workspace.agent_pool != null
-    ])
-    if try(project.workspaces, null) != null
+      #flatten([for tfc_workspace_key, tfc_workspace in workspace.tfc_workspace :
+        try(workspace.tfc_workspace.agent_pool, null)
+      #]) if try(workspace.tfc_workspace, null) != null
+    ]) if try(project.workspaces, null) != null
   ])
 
 }
