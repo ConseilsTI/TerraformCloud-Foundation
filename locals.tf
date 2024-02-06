12839f482c59d1fcbbe8d0cfdc2d3d40ebb428e7
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-06 10:15:05 -0500

    v0.0.22

--- locals.tf
+++ locals.tf
@@ -194,6 +194,7 @@ locals {
         "TerraformCloud-ModulesRegistry" = {
           description = "Repository to provision and manage Terraform Cloud modules registry using Terraform code (IaC)."
           tfc_workspace = {
+            tag_names = ["foundation", "factory"]
             vcs_repo = true
           }
           tfc_notifications = {
