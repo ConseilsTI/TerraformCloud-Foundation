57357b104bc5fbc7ae1d6608ebb3920184dab543
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-06 08:55:30 -0500

    v0.0.22

--- locals_v2.tf
+++ locals_v2.tf
@@ -46,6 +46,7 @@ locals {
 
 locals {
 
+  # This local is use to determin each required GitHub repository.
   repositories = flatten([for directory_key, directory in local.directories :
     flatten([for workspace_key, workspace in directory.workspaces :
       merge(
@@ -57,8 +58,26 @@ locals {
     ])
     if try(directory.workspaces, null) != null
   ])
+
+  # The following locals use logic to determine the project associate with each workspace.
+  v2_workspaces = flatten([for directory_key, directory in local.directories :
+    flatten([for workspace_key, workspace in directory.workspaces :
+      merge(
+        workspace.tfc_workspace,
+        { name = workspace_key
+        description = workspace.description
+        project = projects }
+      ) if try(workspace.tfc_workspace, null) != null
+    ])
+    if try(project.workspaces, null) != null
+  ])
+  
 }
 
 output "repositories" {
   value = local.repositories
+}
+
+output "v2_workspaces" {
+  value = local.v2_workspaces
 }
