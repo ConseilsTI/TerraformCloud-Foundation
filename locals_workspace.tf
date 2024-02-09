e487d63439ae8ab48b8753502056133c00ef8ea9
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-09 10:57:47 -0500

    v0.0.22

--- locals_workspace.tf
+++ locals_workspace.tf
@@ -1,6 +1,6 @@
 locals {
   # The following locals use logic to determine the project associate with each workspace.
-  workspaces = flatten([for project_key, project in local.projects :
+  workspaces = nonsensitive(flatten([for project_key, project in local.projects :
     flatten([for workspace_key, workspace in project.workspaces :
       merge(
         workspace.tfc_workspace,
@@ -9,7 +9,7 @@ locals {
         project = project_key }
       ) if try(workspace.tfc_workspace, null) != null
     ]) if try(project.workspaces, null) != null
-  ])
+  ]))
   # The following locals use logic to determine the remote_consomer_ids.
   # remote_state_consumer_ids = flatten([for project_key, project in local.projects :
   #   flatten([for workspace_key, workspace in project.workspaces :
@@ -18,8 +18,4 @@ locals {
   #   ])
   #   if try(project.workspaces, null) != null
   # ])
-}
-
-output "workspaces" {
-  value = local.workspaces
 }
