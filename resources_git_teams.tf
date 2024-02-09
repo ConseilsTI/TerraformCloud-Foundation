adbc5815c5542223143d931a64546d234f4f5e95
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-09 11:26:50 -0500

    v0.0.22

--- resources_git_teams.tf
+++ resources_git_teams.tf
@@ -1,16 +1,18 @@
 # The following resource block is used to create GitHub team resources.
 
 module "github_teams" {
-  source = "./modules/github_team"
+
+  source = "./modules/git_team"
 
   for_each = nonsensitive({ for team in local.git_teams : team.name => team })
 
   name                      = each.value.name
-  description               = try(each.value.description, null)
-  privacy                   = try(each.value.privacy, "closed")
-  parent_team_id            = try(each.value.parent_team_id, null)
-  ldap_dn                   = try(each.value.ldap_dn, null)
   create_default_maintainer = try(each.value.create_default_maintainer, false)
-  repository                = try(each.value.repository, null)
+  description               = try(each.value.description, null)
+  ldap_dn                   = try(each.value.ldap_dn, null)
+  parent_team_id            = try(each.value.parent_team_id, null)
   permission                = try(each.value.permission, null)
+  privacy                   = try(each.value.privacy, "closed")
+  repository                = try(each.value.repository, null)
+  
 }
