b884885cfa0b7be79986762e1def233722a381be
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-10 11:26:48 -0500

    v0.0.26

--- locals_tfc_team.tf
+++ locals_tfc_team.tf
@@ -2,7 +2,7 @@ locals {
 
   # The following locals use logic to determine the project associate with each team.
   tfc_project_teams = flatten([for project_key, project in local.projects :
-    flatten([for team_key, team in project.tfc_teams :
+    flatten([for team in project.tfc_teams :
       merge(
         team,
         {
@@ -15,7 +15,7 @@ locals {
   # The following locals use logic to determine the workspace associate with each team.
   tfc_workspace_teams = flatten([for project_key, project in local.projects :
     flatten([for component_key, component in project.components :
-      flatten([for team_key, team in component.tfc_teams :
+      flatten([for team in component.tfc_teams :
         merge(
           team,
           {
