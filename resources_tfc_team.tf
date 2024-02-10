271119be21a7fe35353e9ba7d50626744cbc32a6
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-10 10:26:35 -0500

    ft: Team Module

--- resources_tfc_team.tf
+++ resources_tfc_team.tf
@@ -5,23 +5,24 @@ module "tfe_teams" {
 
   for_each = nonsensitive({ for team in local.tfc_teams : team.name => team })
 
-  name                    = each.value.name
-  organization            = data.tfe_organization.this.name
-  visibility              = try(each.value.visibility, "organization")
-  sso_team_id             = try(each.value.sso_team_id, null)
-  organization_access     = try(each.value.organization_access, null)
-  project_name            = try(each.value.project, null)
-  project_id              = try(tfe_project.project[each.value.project].id, null)
-  project_access          = try(each.value.project_access, null)
-  custom_project_access   = try(each.value.custom_project_access, null)
-  custom_workspace_access = try(each.value.custom_workspace_access, null)
-  workspace_name          = try(each.value.workspace, null)
-  workspace_id            = try(module.tfe_workspaces[each.value.workspace].id, null)
-  workspace_access        = try(each.value.workspace_access, null)
-  workspace_permission    = try(each.value.workspace_permission, null)
-  token                   = try(each.value.token, false)
-  token_force_regenerate  = try(each.value.token_force_regenerate, null)
-  token_expired_at        = try(each.value.token_expired_at, null)
-  members                 = try(each.value.members, null)
+  name                        = each.value.name
+  custom_project_access       = try(each.value.custom_project_access, null)
+  custom_workspace_access     = try(each.value.custom_workspace_access, null)
+  organization                = data.tfe_organization.this.name
+  organization_access         = try(each.value.organization_access, null)
+  organization_membership_ids = try(each.value.members, null)
+  project_access              = try(each.value.project_access, null)
+  project_id                  = try(tfe_project.project[each.value.project].id, null)
+  project_name                = try(each.value.project, null)
+  sso_team_id                 = try(each.value.sso_team_id, null)
+  token                       = try(each.value.token, false)
+  token_expired_at            = try(each.value.token_expired_at, null)  
+  token_force_regenerate      = try(each.value.token_force_regenerate, null)
+  visibility                  = try(each.value.visibility, "organization")
+  workspace_access            = try(each.value.workspace_access, null)
+  workspace_id                = try(module.tfe_workspaces[each.value.workspace].id, null)
+  workspace_name              = try(each.value.workspace, null)
+  workspace_permission        = try(each.value.workspace_permission, null)
+  
 }
 
