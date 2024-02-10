0b00209733d6c9ee9de454d577d09b8870a09b07
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-10 10:33:20 -0500

    ft: Agent Module

--- modules/tfe_agent/main.tf
+++ modules/tfe_agent/main.tf
@@ -1,11 +1,16 @@
 resource "tfe_agent_pool" "this" {
+
   name                = var.name
   organization        = var.organization
   organization_scoped = var.organization_scoped
+
 }
 
 resource "tfe_agent_token" "this" {
-  for_each      = toset(var.token_description)
+
+  for_each = toset(var.token_description)
+
   agent_pool_id = tfe_agent_pool.this.id
   description   = each.key
+
 }
