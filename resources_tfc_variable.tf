3c5f332086927c94dd5a8b4d6e9975e3c643fa6d
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-10 11:29:54 -0500

    v0.0.26

--- resources_tfc_variable.tf
+++ resources_tfc_variable.tf
@@ -1,7 +1,7 @@
 # The following code block is used to create variable resources.
 
 resource "tfe_variable" "variable_set" {
-  for_each = nonsensitive({ for variable in local.tfc_variable_sets_variables : "${variable.variable_set} ${variable.name}" => variable })
+  for_each = nonsensitive({ for variable in local.tfc_variable_sets_variables : "${variable.variable_set} ${variable.key}" => variable })
 
   key             = each.value.key
   value           = each.value.key == "TFE_TOKEN" ? try(module.tfe_teams[each.value.value].token, each.value.value) : each.value.value
@@ -13,7 +13,7 @@ resource "tfe_variable" "variable_set" {
 }
 
 resource "tfe_variable" "workspace" {
-  for_each = nonsensitive({ for variable in local.tfc_workspace_variables : "${variable.workspace} ${variable.name}" => variable })
+  for_each = nonsensitive({ for variable in local.tfc_workspace_variables : "${variable.workspace} ${variable.key}" => variable })
 
   key          = each.value.key
   value        = each.value.key == "TFE_TOKEN" ? try(module.tfe_teams[each.value.value].token, each.value.value) : each.value.value
