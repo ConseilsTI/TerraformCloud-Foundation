4225f5ae62e2bb541d140837b2894193698ee778
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-10 10:56:34 -0500

    v0.0.26

--- locals_git_team.tf
+++ locals_git_team.tf
@@ -4,7 +4,13 @@ locals {
 
   git_teams = flatten([for project_key, project in local.projects :
     flatten([for component_key, component in project.components :
-      flatten([for team in component.git_teams : team
+      flatten([for team in component.git_teams : 
+        merge(
+          team,
+          {
+            repository = component_key
+          }
+        )
       ]) if try(component.git_teams, null) != null
     ]) if try(project.components, null) != null
   ])
