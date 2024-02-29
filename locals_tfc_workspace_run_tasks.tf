378c8ec62c739159ecdb3a39acb9e57ffbe20366
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-02-29 16:22:35 -0500

    v0.0.29

--- locals_tfc_workspace_run_tasks.tf
+++ locals_tfc_workspace_run_tasks.tf
@@ -8,6 +8,6 @@ locals {
   ])
 
   # The following locals is used to concat the default run tasks with every run tasks assign to a specefic workspace.
-  tfc_run_tasks = concat([for value in local.local.tfc_default_run_tasks : value.name], local.tfc_workspace_run_tasks)
+  tfc_run_tasks = concat([for value in local.tfc_default_run_tasks : value.name], local.tfc_workspace_run_tasks)
 
 }
