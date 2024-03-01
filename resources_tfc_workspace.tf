f3170bf92f7ccee6f79cf3d24e409d27d3c117de
GitHub Action <notifications@github.com>
2024-03-01 12:53:17 +0000

    Automated Terraform Formatting

--- resources_tfc_workspace.tf
+++ resources_tfc_workspace.tf
@@ -5,22 +5,22 @@ module "tfe_workspaces" {
 
   for_each = nonsensitive({ for workspace in local.tfc_workspaces : workspace.name => workspace })
 
-  name                          = each.value.name
-  agent_pool_id                 = try(module.tfe_agent[each.value.agent_pool].agent_pool_id, null)
-  allow_destroy_plan            = try(each.value.allow_destroy_plan, null)
-  assessments_enabled           = try(each.value.assessments_enabled, null)
-  auto_apply                    = try(each.value.auto_apply, null)
-  auto_apply_run_trigger        = try(each.value.auto_apply_run_trigger, null)
-  description                   = try(each.value.description, null)
-  execution_mode                = try(each.value.execution_mode, null)
-  file_triggers_enabled         = try(each.value.file_triggers_enabled, null)
-  global_remote_state           = try(each.value.global_remote_state, null)
-  organization                  = data.tfe_organization.this.name
-  project_id                    = tfe_project.project[each.value.project].id
-  queue_all_runs                = try(each.value.queue_all_runs, null)
-  remote_state_consumer_ids     = try([for value in each.value.remote_state_consumer_ids : data.tfe_workspace.this[value].id], null)
-  run_tasks = flatten([for value in concat(local.tfc_default_run_tasks, try(each.value.run_tasks, [])) : 
-    { 
+  name                      = each.value.name
+  agent_pool_id             = try(module.tfe_agent[each.value.agent_pool].agent_pool_id, null)
+  allow_destroy_plan        = try(each.value.allow_destroy_plan, null)
+  assessments_enabled       = try(each.value.assessments_enabled, null)
+  auto_apply                = try(each.value.auto_apply, null)
+  auto_apply_run_trigger    = try(each.value.auto_apply_run_trigger, null)
+  description               = try(each.value.description, null)
+  execution_mode            = try(each.value.execution_mode, null)
+  file_triggers_enabled     = try(each.value.file_triggers_enabled, null)
+  global_remote_state       = try(each.value.global_remote_state, null)
+  organization              = data.tfe_organization.this.name
+  project_id                = tfe_project.project[each.value.project].id
+  queue_all_runs            = try(each.value.queue_all_runs, null)
+  remote_state_consumer_ids = try([for value in each.value.remote_state_consumer_ids : data.tfe_workspace.this[value].id], null)
+  run_tasks = flatten([for value in concat(local.tfc_default_run_tasks, try(each.value.run_tasks, [])) :
+    {
       enforcement_level = try(value.enforcement_level, "advisory")
       stage             = try(value.stage, "post_plan")
       task_id           = data.data.tfe_organization_run_task.this[value.name].id
