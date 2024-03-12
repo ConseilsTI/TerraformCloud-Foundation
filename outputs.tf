a2c9d8d6af99da56884fddc69d2fec9ebf4fbd41
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-03-12 15:49:47 -0400

    v0.0.34

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "modules_registry_github_owners_team" {
   description = "The name of the GitHub team who owns the private modules registry."
-  value       = module.git_teams["TerraformCloud-ModulesRegistry-Owners"].name
+  value       = module.git_teams["TerraformCloud-ModulesRegistry-Owners"].team.name
 }
 
 output "modules_registry_github_contributors_team" {
   description = "The name of the GitHub team who can contribute to the private modules registry."
-  value       = module.git_teams["TerraformCloud-ModulesRegistry-Contributors"].name
+  value       = module.git_teams["TerraformCloud-ModulesRegistry-Contributors"].team.name
 }
