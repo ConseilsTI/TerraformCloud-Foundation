3b95112a0cc478f63284113ddd35b7c375d7a0df
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-03-19 09:34:28 -0400

    v0.0.46

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,7 @@ output "hcp_vault_secrets_app_name" {
 output "manage_modules_team_token" {
   description = "The token of the team with `manage-modules` access."
   value       = module.tfe_teams["manage-modules"].token
+  sensitive   = true
 }
 
 output "modules_registry_github_contributors_team" {
