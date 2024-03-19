5b216ad24e141427525d6c8fba70182f6ae5a213
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-03-19 08:05:16 -0400

    v0.0.45

--- resources_hcp_vault_secrets.tf
+++ resources_hcp_vault_secrets.tf
@@ -1,7 +1,7 @@
 # The following code blode is used to create secret in Hashicorp Vault.
 
 resource "hcp_vault_secrets_secret" "this" {
-  for_each     = nonsensitive({ for team in local.tfc_teams : team.name => team if team == true })
+  for_each     = nonsensitive({ for team in local.tfc_teams : team.name => team if team.token == true })
   app_name     = "TerraformCloud"
   secret_name  = "TFC_API_TOKEN_${lower(replace(each.value.name, "/\\W|_|\\s/", "_"))}"
   secret_value = module.tfe_teams[each.value.name].token
